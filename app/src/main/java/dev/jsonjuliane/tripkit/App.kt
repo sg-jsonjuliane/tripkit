aeca5bb32f2953735635cdedc2f45a78add007f6
sgjsonjuliane <jason@skedgo.com>
2023-10-31 16:43:33 +0800

    INIT: Initial tripkit project

---
+++ app/src/main/java/dev/jsonjuliane/tripkit/App.kt
@@ -0,0 +1,29 @@
+package dev.jsonjuliane.tripkit
+
+import android.app.Application
+import com.skedgo.DaggerTripKit
+import com.skedgo.TripKit
+import com.skedgo.tripkit.HttpClientModule
+import com.skedgo.tripkit.MainModule
+import com.skedgo.tripkit.TripKitConfigs
+import com.skedgo.tripkit.configuration.Key
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val configs = TripKitConfigs.builder().context(this)
+            .debuggable(true)
+            .key { Key.ApiKey(getString(R.string.skedgo_api_key)) }
+            .build()
+
+        val httpClientModule = HttpClientModule(null, null, configs)
+
+        val tripKit = DaggerTripKit.builder()
+            .mainModule(MainModule(configs))
+            .httpClientModule(httpClientModule)
+            .build()
+
+        TripKit.initialize(this, tripKit)
+    }
+}
