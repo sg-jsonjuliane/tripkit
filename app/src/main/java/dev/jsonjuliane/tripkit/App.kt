035100d02e13b34160772b445b3bda8ff872dddf
sgjsonjuliane <jason@skedgo.com>
2023-11-06 20:31:20 +0800

    UPT: Working base tripkit map fragment

--- app/src/main/java/dev/jsonjuliane/tripkit/App.kt
+++ app/src/main/java/dev/jsonjuliane/tripkit/App.kt
@@ -1,29 +1,46 @@
 package dev.jsonjuliane.tripkit
 
 import android.app.Application
-import com.skedgo.DaggerTripKit
-import com.skedgo.TripKit
+import com.skedgo.tripkit.DateTimePickerConfig
 import com.skedgo.tripkit.HttpClientModule
-import com.skedgo.tripkit.MainModule
 import com.skedgo.tripkit.TripKitConfigs
 import com.skedgo.tripkit.configuration.Key
+import com.skedgo.tripkit.ui.TripKitUI
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        val configs = TripKitConfigs.builder().context(this)
-            .debuggable(true)
-            .key { Key.ApiKey(BuildConfig.SKEDGO_KEY) }
+        val baseConfig = TripKitUI.buildTripKitConfig(
+            applicationContext,
+            Key.ApiKey(BuildConfig.SKEDGO_KEY)
+        )
+
+        val httpClientModule = HttpClientModule(
+            null,
+            BuildConfig.VERSION_NAME,
+            baseConfig,
+            null
+        )
+
+        val dateTimePickerConfig = DateTimePickerConfig(
+            "Leave at",
+            "Arrive By"
+        )
+
+        val appConfigs = TripKitConfigs.builder()
+            .from(baseConfig)
+            .dateTimePickerConfig(dateTimePickerConfig)
+            .hasGetOffAlerts(false)
             .build()
 
-        val httpClientModule = HttpClientModule(null, null, configs)
+        TripKitUI.initialize(
+            this,
+            Key.ApiKey(BuildConfig.SKEDGO_KEY),
+            appConfigs,
+            httpClientModule,
+            getString(R.string.MAPS_KEY)
+        )
 
-        val tripKit = DaggerTripKit.builder()
-            .mainModule(MainModule(configs))
-            .httpClientModule(httpClientModule)
-            .build()
-
-        TripKit.initialize(this, tripKit)
     }
 }
--- app/src/main/java/dev/jsonjuliane/tripkit/MapActivity.kt
+++ app/src/main/java/dev/jsonjuliane/tripkit/MapActivity.kt
@@ -1,15 +1,27 @@
 package dev.jsonjuliane.tripkit
 
 import android.os.Bundle
+import android.os.Handler
+import com.skedgo.geocoding.LatLng
 import com.skedgo.tripkit.ui.core.BaseActivity
 import com.skedgo.tripkit.ui.map.home.TripKitMapFragment
 import dev.jsonjuliane.tripkit.databinding.ActivityMapBinding
 
 class MapActivity : BaseActivity<ActivityMapBinding>() {
+
     override val layoutRes: Int
         get() = R.layout.activity_map
 
     override fun onCreated(instance: Bundle?) {
-        // Intentionally left empty for the mean time
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.mapFragment) as TripKitMapFragment
+        mapFragment.getMapAsync { _ ->
+            Handler().postDelayed({
+                mapFragment.moveToLatLng(
+                    LatLng(-27.470125, 153.021072)
+                )
+            }, 2000)
+
+        }
     }
 }
